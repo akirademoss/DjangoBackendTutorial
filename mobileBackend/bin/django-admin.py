7b63f726ec5b0bfc0976c841e8d34282ca54eee5
Akira <akirademoss@gmail.com>
2018-11-23 21:01:23 -0800

    Committing project to repo

---
+++ mobileBackend/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mclovin/Projects/x3ThirdProject/mobileBackend/mobileBackend/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
